d9fe5665077b2b942f5862c583b0b6ebb394a78a
ahughes <ahughes@d2.com>
2017-08-22 16:58:29 -0700

    Making my attribtes replacement support multiline. Turns out it didn't. Weird.

--- src/python/qt_py_convert/run.py
+++ src/python/qt_py_convert/run.py
@@ -66,7 +66,8 @@ def _convert_attributes(red, aliases):
                 r"^(?P<module>{modules})\.(?P<widget>(?:{widgets})(?:[\.\[\(].*)?)$".format(             # Regular expression
                     modules="|".join(re.escape(name) for name in Qt._common_members.keys()),
                     widgets="|".join(re.escape(widget) for widget in Qt._common_members[module_name])
-                )
+                ),
+                re.MULTILINE
             ),
             module_name
         )
@@ -294,10 +295,11 @@ if __name__ == "__main__":
     # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/ddg/trunk/src/python", recursive=True, write=True)
     # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/ddqt/trunk/src/python", recursive=True, write=True)
     # process_file("/dd/shows/DEVTD/user/work.ahughes/svn/packages/nukepipeline/branches/nukepipeline_5/src/nuke/nodes/nukepipeline/ShotLook/shot_look.py", write=True)
-    process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/lightpipeline/trunk/src/python/lightpipeline/ui", recursive=True, write=True, fast_exit=True)
+    # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/lightpipeline/trunk/src/python/lightpipeline/ui", recursive=True, write=True, fast_exit=True)
     # process_file("/dd/shows/DEVTD/user/work.ahughes/svn/packages/lightpipeline/trunk/src/python/lightpipeline/ui/errorDialogUI.py", write=True, fast_exit=True)
     # process_file("/dd/shows/DEVTD/user/work.ahughes/svn/packages/lightpipeline/trunk/src/python/lightpipeline/ui/HDRWidgetComponents.py", write=True, fast_exit=True)
     # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/nukepipeline/branches/nukepipeline_5/src/", recursive=True, write=True, fast_exit=True)
     # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/assetbrowser/trunk/src", recursive=True, write=True)
     # folder = os.path.abspath("../../../../tests/sources")
     # process_folder(folder, recursive=True, write=True)
+    process_file("/dd/shows/DEVTD/user/work.ahughes/svn/packages/atomic/trunk/src/python/atomic/Atomic/ImportExport.py", write=True, fast_exit=False)
