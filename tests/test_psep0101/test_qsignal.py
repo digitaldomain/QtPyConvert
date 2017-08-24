f42236f4166c50707abf85143ad8a12350214a19
ahughes <ahughes@d2.com>
2017-08-24 15:57:19 -0700

    Adding a bunch of disconnect tests as well as making a mock testrunner in the  block.

--- tests/test_psep0101/test_qsignal.py
+++ tests/test_psep0101/test_qsignal.py
@@ -9,6 +9,10 @@ def check_emit(source, dest):
     assert _qsignal.process_emit(source) == dest
 
 
+def check_disconnect(source, dest):
+    assert _qsignal.process_disconnect(source) == dest
+
+
 def test_connection_no_args():
     check_connection(
         'self.connect(self.filterBox, QtCore.SIGNAL("textChanged()"), self.slot_filterBoxEdited)',
@@ -69,5 +73,63 @@ def test_emit_multi_args():
         "self.dagEditor.nodeNameEdited.emit(node, newName)"
     )
 
+def test_disconnect_no_args():
+    check_disconnect(
+        'self.disconnect(self, QtCore.SIGNAL("textChanged()"), self.slot_textChanged)',
+        "self.textChanged.disconnect(self.slot_textChanged)"
+    )
+
+def test_disconnect_single_arg_newlines():
+    check_disconnect(
+        'self.disconnect(self.batchGlobalsNode,\n                            QtCore.SIGNAL("attributeChanged(PyQt_PyObject)"),\n                            self.slot_raceGlobalsAttributeChanged)',
+        "self.batchGlobalsNode.attributeChanged.disconnect(self.slot_raceGlobalsAttributeChanged)"
+    )
+
+def test_disconnect_local_var_newlines():
+    check_disconnect(
+        'self.disconnect(validation_message, QtCore.SIGNAL("atomicPreflightChangedFrameRange()"),\n                            self.slot_clientFrameRangeChanged)',
+        "validation_message.atomicPreflightChangedFrameRange.disconnect(self.slot_clientFrameRangeChanged)"
+    )
+
+def test_disconnect_no_arg_alt():
+    check_disconnect(
+        'self.disconnect(self, QtCore.SIGNAL("atomicLevelEnvironmentChanged()"), self._slotLevelEnvironmentChanged)',
+        "self.atomicLevelEnvironmentChanged.disconnect(self._slotLevelEnvironmentChanged)"
+    )
+
+def test_disconnect_single_arg():
+    check_disconnect(
+        'self.disconnect(self.sceneDag, QtCore.SIGNAL("dagChanged(PyQt_PyObject)"), self._slotDagChanged)',
+        "self.sceneDag.dagChanged.disconnect(self._slotDagChanged)"
+    )
+
+
 if __name__ == "__main__":
-    test_emit_multi_args()
+    _tests = filter(
+        lambda key: True if key.startswith("test_") else False,
+        globals().keys()
+    )
+
+    failed = []
+    for test in _tests:
+        try:
+            print("Running %s" % test)
+            globals()[test]()
+            print("    %s succeeded!" % test)
+        except AssertionError as err:
+            print("    %s failed!" % test)
+            failed.append((test, err))
+        print("")
+    for failure_name, failure_error in failed:
+        print("""
+------------ %s FAILED ------------
+%s
+""" % (failure_name, failure_error))
+
+    print(
+        "\n\n%d failures, %d success, %s%%" % (
+            len(failed),
+            len(_tests)-len(failed),
+            "%.1f" % ((float(len(_tests)-len(failed))/len(_tests))*100)
+        )
+    )
