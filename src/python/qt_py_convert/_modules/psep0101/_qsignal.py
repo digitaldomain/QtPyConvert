f2dac09a10f9677613c856380070210ce03d65a1
ahughes <ahughes@d2.com>
2017-08-24 15:55:37 -0700

    Adding disconnect support

--- src/python/qt_py_convert/_modules/psep0101/_qsignal.py
+++ src/python/qt_py_convert/_modules/psep0101/_qsignal.py
@@ -10,6 +10,13 @@ def _connect_repl(match_obj):
     return template.format(**groups)
 
 
+def _disconnect_repl(match_obj):
+    template = r"{owner}.{signal}.disconnect({slot})"
+    groups = match_obj.groupdict()
+    groups["args"] = parse_args(groups["args"])
+    return template.format(**groups)
+
+
 def _emit_repl(match_obj):
     template = r"{owner}.{signal}.emit({args})"
     groups = match_obj.groupdict()
@@ -19,7 +26,7 @@ def _emit_repl(match_obj):
 
 def process_connect(function_str):
     SIGNAL_RE = re.compile(
-        r"(?:self\.)?connect\((?:\s+)?(?P<owner>.*>?),(?:\s+)?(?:QtCore\.)?SIGNAL\((?:\s+)?[\'\"](?P<signal>\w+)\((?P<args>.*?)\)[\'\"](?:\s+)?\),(?:\s+)?(?P<slot>.*?)\)"
+        r"(?P<root>\w+)?\.connect\((?:\s+)?(?P<owner>.*>?),(?:\s+)?(?:QtCore\.)?SIGNAL\((?:\s+)?[\'\"](?P<signal>\w+)\((?P<args>.*?)\)[\'\"](?:\s+)?\),(?:\s+)?(?P<slot>.*?)\)"
     )
     replacement_str = SIGNAL_RE.sub(
         _connect_repl,
@@ -30,9 +37,30 @@ def process_connect(function_str):
     return function_str
 
 
+def process_disconnect(function_str):
+    SIGNAL_RE = re.compile(
+        r"(?P<root>\w+)?\.disconnect\((?:\s+)?(?P<owner>.*>?),(?:\s+)?(?:QtCore\.)?SIGNAL\((?:\s+)?[\'\"](?P<signal>\w+)\((?P<args>.*?)\)[\'\"](?:\s+)?\),(?:\s+)?(?P<slot>.*?)\)"
+    )
+
+    # SIGN = re.compile(
+    #     r"(?P<root>\w+)?\.disconnect\((?:\s+)?(?P<owner>.*?),(?:\s+)?(?:QtCore\.)?SIGNAL\((?:\s+)?[\'\"](?P<signal>\w+)\((?P)"
+    # )
+    """
+    'self.disconnect(self, QtCore.SIGNAL("textChanged()"), self.slot_textChanged)',
+    "self.textChanged.disconnect(self.slot_textChanged)"
+    """
+    replacement_str = SIGNAL_RE.sub(
+        _disconnect_repl,
+        function_str
+    )
+    if replacement_str != function_str:
+        return replacement_str
+    return function_str
+
+
 def process_emit(function_str):
     SIGNAL_RE = re.compile(
-        r"(?:(?P<owner>\w+)\.)?emit\((?:\s+)?(?:QtCore\.)?SIGNAL\((?:\s+)?[\"\'](?P<signal>\w+)\((?P<arg_types>.*?)\)[\"\'](?:\s+)?\)(?:\s+)?(?:,(?:\s+)?)?(?P<args>.*?)\)"
+        r"(?P<owner>\w+)?\.emit\((?:\s+)?(?:QtCore\.)?SIGNAL\((?:\s+)?[\"\'](?P<signal>\w+)\((?P<arg_types>.*?)\)[\"\'](?:\s+)?\)(?:\s+)?(?:,(?:\s+)?)?(?P<args>.*?)\)"
     )
     match = SIGNAL_RE.search(function_str)
     replacement_str = SIGNAL_RE.sub(
