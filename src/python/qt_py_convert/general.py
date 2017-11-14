9a89a42347b3cbe2aab67f2c8fd303368a0c721c
ahughes <ahughes@d2.com>
2017-11-14 14:31:38 -0800

    Adding optional fallback to the _color command.

--- src/python/qt_py_convert/general.py
+++ src/python/qt_py_convert/general.py
@@ -1,9 +1,39 @@
-import json
 import copy
+import json
 import os
+import subprocess
+import sys
+
+
+def supports_color():
+    """
+    Returns True if the running system's terminal supports color, and False
+    otherwise.
+    """
+    plat = sys.platform
+    supported_platform = plat != 'Pocket PC' and (plat != 'win32' or
+                                                  'ANSICON' in os.environ)
+    p = subprocess.Popen(
+        ["tput", "colors"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    has_colors = p.communicate()[0].strip("\n")
+    try:
+        has_colors = int(has_colors)
+    except:
+        has_colors = False
+
+    # isatty is not always implemented, #6223.
+    is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
+    if (not supported_platform or not is_a_tty) and not has_colors:
+        return False
+    return True
+
+__supports_color = supports_color()
 
 
 def _color(color, text):
+    if not __supports_color:
+        return text
     return "\033[%dm%s\033[0m" % (color, text)
 
 # Binding support
