80bf19e14946f842d3254459ee4a349ee43df809
Alex Hughes <ahughes@d2.com>
2018-03-01 16:30:18 -0800

    Adding a test for the QApplication.translate problem we were having.

---
+++ tests/test_qtcompat/test_compatibility_members.py
@@ -0,0 +1,57 @@
+from qt_py_convert.run import run
+from qt_py_convert.general import _highlight_diffs
+
+
+def check(source, dest):
+    aliases, mappings, dumps = run(source, True, True)
+    try:
+        assert dumps == dest
+    except AssertionError as err:
+        raise AssertionError("\n\"%s\"\n!=\n\"%s\"\n" %
+            _highlight_diffs(dumps, dest)
+        )
+
+
+def test_qapplication_translate_basic():
+    check(
+        """from PySide import QtGui
+
+QtGui.QApplication.translate(context, text, disambig)
+""",
+        """from Qt import QtCompat
+
+QtCompat.translate(context, text, disambig)
+"""
+    )
+
+
+if __name__ == "__main__":
+    import traceback
+    _tests = filter(
+        lambda key: True if key.startswith("test_") else False,
+        globals().keys()
+    )
+
+    failed = []
+    for test in _tests:
+        try:
+            print("Running %s" % test)
+            globals()[test]()
+            print("    %s succeeded!" % test)
+        except AssertionError as err:
+            print("    %s failed!" % test)
+            failed.append((test, traceback.format_exc()))
+        print("")
+    for failure_name, failure_error in failed:
+        print("""
+------------ %s FAILED ------------
+%s
+""" % (failure_name, failure_error))
+
+    print(
+        "\n\n%d failures, %d success, %s%%" % (
+            len(failed),
+            len(_tests)-len(failed),
+            "%.1f" % ((float(len(_tests)-len(failed))/len(_tests))*100)
+        )
+    )
