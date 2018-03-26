b28aeb4842c0515846d8f51b67cf7633597c4ec8
Alex Hughes <ahughes@d2.com>
2018-03-26 12:52:49 -0700

    Using the broken out convert_mappings function in qt_py_convert.mappings

--- src/python/qt_py_convert/run.py
+++ src/python/qt_py_convert/run.py
@@ -15,6 +15,7 @@ from qt_py_convert.general import merge_dict, _custom_misplaced_members, \
     ALIAS_DICT, change, UserInputRequiredException, ANSI,  \
     __suplimentary_bindings__, WriteMode, is_py, build_exc
 from qt_py_convert.color import color_text
+from qt_py_convert.mappings import convert_mappings
 from qt_py_convert.log import get_logger
 
 COMMON_MODULES = Qt._common_members.keys() + ["QtCompat"]
@@ -430,53 +431,6 @@ def _convert_body(red, aliases, mappings, skip_lineno=False):
                     # match.replace(mappings[key])
 
 
-def _convert_mappings(aliases, mappings):
-    """
-    _convert_mappings will build a proper mapping dictionary using any
-    aliases that we have discovered previously.
-    It builds regular expressions based off of the Qt._common_members and will
-    replace the mappings that are used with updated ones in Qt.py
-
-    :param aliases: Aliases is the replacement information that is build
-        automatically from qt_py_convert.
-    :type aliases: dict
-    :param mappings: Mappings is information about the bindings that are used.
-    :type mappings: dict
-    :return: _convert_mappings will just return the mappings dict,
-        however it is updating the aliases["used"] set.
-    :rtype: dict
-    """
-    expressions = [
-        re.compile(
-            r"(?P<module>{modules})\.(?P<widget>{widgets})$".format(
-                # Regular expression
-                modules="|".join(
-                    re.escape(name) for name in Qt._common_members.keys()
-                ),
-                widgets="|".join(
-                    re.escape(widget) for widget in Qt._common_members[module]
-                )
-            )
-        )
-        for module in Qt._common_members.keys()
-    ]
-    for from_mapping in mappings:
-        iterable = zip(Qt._common_members.keys(), expressions)
-        for module_name, expression in iterable:
-            modified_mapping = expression.sub(
-                r"{module}.\2".format(module=module_name),
-                mappings[from_mapping]
-            )
-            if modified_mapping != mappings[from_mapping]:
-                # Mapping changed
-                # _---------------------------_ #
-                # We shouldn't be adding it here.
-                # We don't know if it's used yet.
-                #       aliases["used"].add(module_name)
-                mappings[from_mapping] = modified_mapping
-    return mappings
-
-
 def misplaced_members(aliases, mappings):
     """
     misplaced_members uses the internal "_misplaced_members" from Qt.py as
@@ -581,7 +535,7 @@ def run(text, skip_lineno=False, tometh_flag=False):
     aliases, mappings = misplaced_members(aliases, mappings)
     aliases["used"] = set()
 
-    mappings = _convert_mappings(aliases, mappings)
+    mappings = convert_mappings(aliases, mappings)
 
     # Convert using the psep0101 module.
     psep0101.process(red, skip_lineno=skip_lineno, tometh_flag=tometh_flag)
@@ -721,7 +675,7 @@ def process_folder(folder, recursive=False, write_mode=None, write_args=None, sk
     # TODO: Might need to parse the text to remove whitespace at the EOL.
     #       #101 at https://github.com/PyCQA/baron documents this issue.
 
-    for fn in filter(_is_py, [os.path.join(folder, fp) for fp in os.listdir(folder)]):
+    for fn in filter(is_py, [os.path.join(folder, fp) for fp in os.listdir(folder)]):
         process_file(
             fn,
             write_mode=write_mode,
