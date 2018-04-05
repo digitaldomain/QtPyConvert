91017c09f947693a07a9c7b6114ec0a4410c92d6
Alex Hughes <ahughes@d2.com>
2018-04-05 14:57:49 -0700

    Adding license to each file

--- src/python/qt_py_convert/run.py
+++ src/python/qt_py_convert/run.py
@@ -1,3 +1,24 @@
+# Copyright 2018 Digital Domain 3.0
+#
+# Licensed under the Apache License, Version 2.0 (the "Apache License")
+# with the following modification; you may not use this file except in
+# compliance with the Apache License and the following modification to it:
+# Section 6. Trademarks. is deleted and replaced with:
+#
+# 6. Trademarks. This License does not grant permission to use the trade
+#    names, trademarks, service marks, or product names of the Licensor
+#    and its affiliates, except as required to comply with Section 4(c) of
+#    the License and to reproduce the content of the NOTICE file.
+#
+# You may obtain a copy of the Apache License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Apache License with the above modification is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the Apache License for the specific
+# language governing permissions and limitations under the Apache License.
 import os
 import re
 import sys
@@ -666,7 +687,7 @@ if __name__ == "__main__":
     # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/ddqt/trunk/src/python/ddqt", recursive=True, write=False, skip_lineno=True, tometh_flag=True)
     # folder = os.path.abspath("../../../../tests/sources")
     # process_folder(folder, recursive=True, write=True)
-    # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/rvplugins/tags/0.19.4/src", recursive=True, write=True, skip_lineno=True, tometh_flag=True)
-    process_file("/dd/shows/DEVTD/user/work.ahughes/svn/packages/refchef/branches/qt_compat/src/python/refchef/tagspanel.py", write_mode=WriteMode.OVERWRITE, tometh_flag=True, explicit_signals_flag=False)
+    process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/jstemplate_explorer/branches/qt_compat/src/bin", recursive=True, write_mode=WriteMode.OVERWRITE, skip_lineno=True, tometh_flag=True)
+    # process_file("/dd/shows/DEVTD/user/work.ahughes/svn/packages/refchef/branches/qt_compat/src/python/refchef/tagspanel.py", write_mode=WriteMode.OVERWRITE, tometh_flag=True, explicit_signals_flag=False)
     # process_folder("/dd/shows/DEVTD/user/work.ahughes/svn/packages/refchef/branches/qt_compat/src/", write_mode=WriteMode.OVERWRITE, tometh_flag=True, recursive=True)
     # process_file("/dd/shows/DEVTD/user/work.ahughes/svn/packages/ddqt/trunk/src/python/ddqt/gui/SnapshotModel.py", write=False, tometh_flag=True)
