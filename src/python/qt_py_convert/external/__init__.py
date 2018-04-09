12a89db861392301dcbb403760ab0cda04a4f0bf
Alex Hughes <ahughes@d2.com>
2018-04-09 14:46:08 -0700

    Removing everything except for the Qt_patched redirection. We want to either stand this up as a fork of mottosso/master or just get it merged in. Until then it is vendor'd.

--- src/python/qt_py_convert/external/__init__.py
+++ src/python/qt_py_convert/external/__init__.py
@@ -1,5 +1,3 @@
-import sys
-import os
-
+# This is a temp hac until we can either stand up our own patched Qt.py
+#   or get these changes merged into mottosso/master.
 from Qt_patched import Qt
-import redbaron
