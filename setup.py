f99bc3b87f7c1bbcef513bfcb19b125975fe2138
Alex Hughes <ahughesalex@gmail.com>
2018-05-08 00:35:49 -0700

    Updating description.

--- setup.py
+++ setup.py
@@ -21,13 +21,88 @@ classifiers = [
             "Topic :: Utilities"
 ]
 
+long_description = """
+QtPyConvert
+===========
+
+-  `Project Goals <#project-goals>`__
+-  `Project Information <#project-information>`__
+-  `Built With <#built-with>`__
+-  `Contributing <#contributing>`__
+-  `Authors <#authors>`__
+-  `License <#license>`__
+
+An automatic Python Qt binding transpiler to the `Qt.py abstraction
+layer <https://github.com/mottosso/Qt.py>`__. It aims to help in your
+modernization of your Python Qt code. QtPyConvert supports the following
+bindings out of the box: \*
+`PyQt4 <https://www.riverbankcomputing.com/software/pyqt/download>`__ \*
+`PySide <http://pyside.github.io/docs/pyside/>`__ \*
+`PyQt5 <https://www.riverbankcomputing.com/software/pyqt/download5>`__
+\* `PySide2 <https://wiki.qt.io/PySide2>`__
+
+It also has experimental support for defining your own bindings. > See
+`customization <#customization>`__ for more information
+
+Project Goals
+-------------
+
+Convert any code using any of the four major Qt for Python bindings into
+the standardized `Qt.py abstraction
+layer <https://github.com/mottosso/Qt.py>`__.
+
+Warn users about incompatibilities or unsupported code. (WIP)
+
+Standardize Qt imports to maintain sanity in code comprehension. >
+Removing start imports and deep class/module imports
+
+Project Information
+-------------------
+
+Built With
+~~~~~~~~~~
+
+-  `Qt.py <https://github.com/mottosso/Qt.py>`__ - The Qt abstraction
+   library that we port code to.
+-  `RedBaron <https://github.com/PyCQA/Redbaron>`__ - The alternate
+   Python AST which allows us to modify and preserve comments +
+   formatting.
+
+Contributing
+~~~~~~~~~~~~
+
+Please read
+`CONTRIBUTING.md <https://github.com/DigitalDomain/QtPyConvert/blob/master/CONTRIBUTING.md>`__
+for details on our code of conduct, and the process for submitting pull
+requests to us.
+
+Authors
+~~~~~~~
+
+-  **Alex Hughes** - Initial work - `Digital
+   Domain <https://digitaldomain.com>`__
+-  **Rafe Sacks** - Prototyping help - `Digital
+   Domain <https://digitaldomain.com>`__
+
+See also the list of
+`contributors <https://github.com/DigitalDomain/QtPyConvert/contributors>`__
+who participated in this project.
+
+License
+~~~~~~~
+
+This project is licensed under the MIT License - see the
+`LICENSE <https://github.com/DigitalDomain/QtPyConvert/blob/master/LICENSE>`__
+file for details
+"""
+
 print("Finding packages: %s" % find_packages("src/python"))
 
 setup(
-    name="QtPyConvert",
+    name="QtPyConvertTEST8",
     version=version,
-    description="An automatic Python Qt binding transpiler to the Qt.py abstraction layer. It aims to help in your modernization of your Python Qt code. QtPyConvert supports the following bindings out of the box:\n    PyQt4\n    PySide\n    PyQt5\n    PySide2\nIt also has experimental support for defining your own bindings.",
-    long_description=open("README.md").read(),
+    description="An automatic Python Qt binding transpiler to the Qt.py abstraction layer. It aims to help in your modernization of your Python Qt code. QtPyConvert supports the following bindings out of the box: PyQt4, PySide, PyQt5, PySide2. It also has experimental support for defining your own bindings.",
+    long_description=long_description,
     author="Alex Hughes",
     author_email="ahughesalex@gmail.com",
     url="https://github.com/digitaldomain/QtPyConvert",
